836281ffb0cea0e2bca87e2cf72c5d405530e921
prejrdev <prejrdev@gmail.com>
2020-10-10 10:12:14 -0400

    back tick syntax

--- day-1.hs
+++ day-1.hs
@@ -13,7 +13,7 @@ to_even n = (if (mod n 2) == 0 then n else n + 1)
 -- so to make use of this, it changes the style
 -- of constructing the if-else expression:l 
 
-to_odd_one n = (if (mod n 2) == 0 then n else n + 1) + 1
+to_odd_one n = (if (n `mod` 2) == 0 then n else n + 1) + 1
 
 -- sentence constructor using lists
 
@@ -31,5 +31,5 @@ filter512 list = [x | x <- list, x `mod` 512 == 0]
 filter'512 list = [x | x <- list, x `mod` 512 /= 0]
 
 --a more complete example of list comprehesion. 
+right_tr_perimeter_solver :: (Num c, Eq c, Enum c) => c -> [(c, c, c)]
 right_tr_perimeter_solver p  = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == p]
-
