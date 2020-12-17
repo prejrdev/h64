5a1cfc18fad9164122763fd58273af1cc0582070
prejrdev <prejrdev@gmail.com>
2020-12-17 12:34:12 -0500

    Update day-1.hs

--- day-1.hs
+++ day-1.hs
@@ -33,4 +33,4 @@ filter'512 list = [x | x <- list, x `mod` 512 /= 0]
 
 --a more complete example of list comprehesion. 
 right_tr_perimeter_solver :: (Num c, Eq c, Enum c) => c -> [(c, c, c)]
-right_tr_perimeter_solver p  = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == p]
+right_tr_perimeter_solver p  = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == p] -- for a solver it literally iterates through everything.
